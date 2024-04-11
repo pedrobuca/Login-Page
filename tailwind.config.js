92ac1f19b48e70acadd861c1b886a31ef818f500
Pedro Buca <pedromateusbuca@gmail.com>
2024-04-11 11:58:21 +0100

    Finalizando o projeto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      margin:{
+       main: '96px',
+      }
+    },
+   
+  },
+  plugins: [],
+}
